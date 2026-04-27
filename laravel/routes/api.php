dbb024b9be72b7283c6f74f4c5e36ec487a217ea
eng-raghadsami <raghadsami.w@gmail.com>
2026-04-27 13:59:05 +0300

    Revert "chore(api): add CORS config and /api/ping connectivity route"
    This reverts commit 5bcae19debab8f23399ce451850c38e14c1d9e55.

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,8 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SimulationController;
 
 Route::post('/simulate', [SimulationController::class, 'simulate']);
-// Simple connectivity check for frontend
-Route::get('/ping', function () {
-	return response()->json(['pong' => true, 'time' => now()]);
-});
 // مثال
